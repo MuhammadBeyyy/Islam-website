272c325ccf60ce016dc66b1cc75708a1e1bd3954
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2023-12-28 05:30:00 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles.css";
 
-//Import Pages
+//Import Pages component
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Error from "./Pages/Error";
@@ -11,7 +11,7 @@ import Rasul from "./Pages/Rasul";
 import Months from "./Pages/Months";
 import Names from "./Pages/Names";
 
-//Import Components
+//Import Components from components section
 import Navbar from "./Components/Navbar";
 import SearchQuran from "./Components/SearchQuran";
 //import Loading from "./Components/Loading";
