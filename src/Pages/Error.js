b7b7abc8ced2d822202c6f8b8c42adacf9719ec4
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2023-12-28 05:31:15 -0800

    Update Error.js

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -5,7 +5,8 @@ export default function Error() {
   return (
     <section className="error-page section">
       <div className="error-container">
-        <h1>oops! it's a dead end</h1>
+        <h1>oops! it's a dead end!</h1>
+        <p> May Allah guide you...</p>
         <Link to="/" className="btn btn-primary">
           back home
         </Link>
