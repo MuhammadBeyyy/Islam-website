5e311bb3b10b4a9b821c2473dea895555c269569
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2024-01-18 07:19:28 -0800

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <h5>Copyright 2023 @Mbey</h5>
+      <h5>Copyright 2024 @Mbey</h5>
       <h5> Contact us: muhammadshittabey@gmail.com</h5>
     </div>
   );
